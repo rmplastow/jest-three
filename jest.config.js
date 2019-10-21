c02555dedd1f8770811ce3df88f68ba61f73bbd5
Bartozzz <bartosz.laniewski@netguru.com>
2019-10-21 18:38:14 +0200

    Update package name and version

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,13 @@
 module.exports = {
+  moduleFileExtensions: ["ts", "js"],
+
   transform: {
-    ".(ts|tsx)": "ts-jest"
+    ".ts": "ts-jest"
   },
+
   testEnvironment: "node",
   testRegex: "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
-  moduleFileExtensions: ["ts", "tsx", "js"],
+
   coveragePathIgnorePatterns: ["/node_modules/", "/test/"],
-  coverageThreshold: {
-    global: {
-      branches: 90,
-      functions: 95,
-      lines: 95,
-      statements: 95
-    }
-  },
   collectCoverageFrom: ["src/*.{js,ts}"]
 };
