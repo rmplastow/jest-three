989eb84c3bcebf5e1823ff54026a5553999f3260
Bartozzz <bartosz.laniewski@netguru.com>
2019-10-21 19:49:37 +0200

    Minimize bundle size

--- rollup.config.ts
+++ rollup.config.ts
@@ -3,6 +3,7 @@ import commonjs from "rollup-plugin-commonjs";
 import sourceMaps from "rollup-plugin-sourcemaps";
 import camelCase from "lodash.camelcase";
 import typescript from "rollup-plugin-typescript2";
+import { terser } from "rollup-plugin-terser";
 import json from "rollup-plugin-json";
 
 const pkg = require("./package.json");
@@ -13,7 +14,7 @@ export default {
     { file: pkg.main, name: camelCase(pkg.name), format: "umd", sourcemap: true },
     { file: pkg.module, format: "es", sourcemap: true }
   ],
-  external: [],
+  external: ["three"],
   watch: {
     include: "src/**"
   },
@@ -22,6 +23,7 @@ export default {
     typescript({ useTsconfigDeclarationDir: true }),
     commonjs(),
     resolve(),
-    sourceMaps()
+    sourceMaps(),
+    terser()
   ]
 };
